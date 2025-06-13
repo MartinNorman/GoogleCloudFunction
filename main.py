36483f0c4447ae86cd35c50c6ef1421de4402368
Martin Norman <martinnorman1973@gmail.com>
2025-06-13 10:40:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,102 +1,105 @@
 import functions_framework
 import requests
-import sqlalchemy
-from sqlalchemy import insert
-from sqlalchemy import text
-from datetime import datetime
+from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 import time
+from google.cloud import bigquery
 
 
 @functions_framework.http
 def test(request):
-##############Connection DB###################################
-    connection_name = "riksbanken:europe-north2:riksbanken-001"
-    db_name = "fx"
-    db_user = "fx"
-    db_password = "MNOmno001"
-
-    driver_name = 'mysql+pymysql'
-    query_string = dict({"unix_socket": "/cloudsql/{}".format(connection_name)})
-
-    db = sqlalchemy.create_engine(
-      sqlalchemy.engine.url.URL(
-        drivername=driver_name,
-        database=db_name,
-        username=db_user,
-        password=db_password,
-        query=query_string,
-      ),
-      pool_size=5,
-      max_overflow=2,
-      pool_timeout=30,
-      pool_recycle=1800
-    )
-
 #######################################################################################
 ## Figure out latest values
 #######################################################################################
 
     def get_latest_date(rate_to_get):
-        with db.connect() as conn:
-            sql = "SELECT max(from_date) as latest FROM exchangerates WHERE currency = %s"
-            latest_value = conn.execute(sql, (rate_to_get))
+
+        # Define the parameter
+        job_config = bigquery.QueryJobConfig(
+           query_parameters=[
+                bigquery.ScalarQueryParameter("currency", "STRING", rate_to_get)
+            ]
+        )
+
+        query = """SELECT max(from_date) as latest FROM `fx.exchangerates` WHERE currency = @currency"""
+        query_job = client.query(query, job_config=job_config)
+        results = query_job.result()
  
-            latest_value_string = str(latest_value.fetchall())
-            date_part_string = latest_value_string[16:-4]
-            if date_part_string == '':
-                date_part_string = str("2021-01-01")
-                date_format = '%Y-%m-%d'
-                convert_date_part_string_to_date_object = datetime.strptime(date_part_string, date_format)
-                from_date = convert_date_part_string_to_date_object
-                return from_date.strftime('%Y-%m-%d')
-            else:
-                date_format = '%Y, %m, %d'
-                convert_date_part_string_to_date_object = datetime.strptime(date_part_string, date_format) 
-                first_date_of_month = convert_date_part_string_to_date_object.replace(day=1)
-                plus_one_month_object = first_date_of_month + relativedelta(months=1)
-                from_date = plus_one_month_object.strftime('%Y-%m-%d')    
-                return str(from_date)
+        latest_value_string = str(next(results))
+        date_part_string = latest_value_string[19:-19]
+        if date_part_string == '':
+            date_part_string = str("2021-01-01")
+            date_format = '%Y-%m-%d'
+            convert_date_part_string_to_date_object = datetime.strptime(date_part_string, date_format)
+            from_date = convert_date_part_string_to_date_object
+            return from_date.strftime('%Y-%m-%d')
+        else:
+            date_format = '%Y, %m, %d'
+            convert_date_part_string_to_date_object = datetime.strptime(date_part_string, date_format) 
+            first_date_of_month = convert_date_part_string_to_date_object.replace(day=1)
+            plus_one_month_object = first_date_of_month + relativedelta(months=1)
+            from_date = plus_one_month_object.strftime('%Y-%m-%d')    
+            return str(from_date)
 #######################################################################################
 ## Query Riksbanken
 #######################################################################################
-    exchangerates_to_get = ['AUD','CAD','EUR','GBP','MXN','NZD','SGD','USD','DKK','JPY','NOK']
-    
+#
+    exchangerates_to_get = ['CAD','AUD','EUR','GBP','MXN','NZD','SGD','USD','DKK','JPY','NOK']
+    #Connect to BigQuery
+    client = bigquery.Client()
+    #Settings for project, schema and table
+    table_id = "riksbanken.fx.exchangerates"
+    #create empty array to be able to store the results of the different queries
     result = []
     for rate_to_get in exchangerates_to_get:
-        
         seriesId = 'SEK' + rate_to_get + 'PMI'
         from_date = get_latest_date(str(rate_to_get))
-#        return 'https://api.riksbank.se/swea/v1/ObservationAggregates/' + seriesId + '/M/' + from_date   
+        #Call Riksbanken
         response = requests.get('https://api.riksbank.se/swea/v1/ObservationAggregates/' + seriesId + '/M/' + from_date)
-    
+        #Handle the response
         if str(response) != "<Response [204]>":        
             response_json = response.json()
-            if len(response_json) > 0:
-                values = [{ 'currency': rate_to_get,
-                    'from_date': response_json[value]["from"], 
-                    'to_date': response_json[value]["to"], 
-                    'average_value': response_json[value]["average"], 
-                    'min_value': response_json[value]["min"], 
-                    'max_value': response_json[value]["max"], 
-                    'ultimo_value': response_json[value]["ultimo"]} 
-                for value in range(0, len(response_json), 1)]
-    
+            #We are getting the first and last WORKINGday of the month but we want the first and last CALENDARday of the month so here we are adjusting this            
+            new_json = []
+            for value in range(0, len(response_json), 1):
+                date_format = '%Y-%m-%d'
+                convert_from_date_part_string_to_date_object = datetime.strptime(response_json[value]["from"], date_format) 
+                first_date_of_month = convert_from_date_part_string_to_date_object.replace(day=1)
+                from_date = first_date_of_month.strftime('%Y-%m-%d')
+
+                convert_to_date_part_string_to_date_object = datetime.strptime(response_json[value]["to"], date_format) 
+
+                next_month = convert_to_date_part_string_to_date_object.replace(day=28) + timedelta(days=4)
+                to_date = (next_month - timedelta(days=next_month.day)).strftime('%Y-%m-%d')
+
+                currency = rate_to_get
+                average_value = response_json[value]["average"]
+                min_value = response_json[value]["min"] 
+                max_value = response_json[value]["max"] 
+                ultimo_value = response_json[value]["ultimo"]
+
+                json = {"currency": currency, "from": from_date, "to": to_date, "average": average_value, "min": min_value, "max": max_value, "ultimo": ultimo_value }
+                new_json.append(json)
+#            return str(new_json)
+
+            if len(new_json) > 0:
+                values = [{ "currency": new_json[value]["currency"],
+                    "from_date": new_json[value]["from"], 
+                    "to_date": new_json[value]["to"], 
+                    "average_value": round(new_json[value]["average"], 4), 
+                    "min_value": round(new_json[value]["min"], 4), 
+                    "max_value": round(new_json[value]["max"], 4), 
+                    "ultimo_value": round(new_json[value]["ultimo"], 4)} 
+                for value in range(0, len(new_json), 1)]
+#                return str(values)
                 try:
-                    with db.connect() as conn:
-                        conn.execute(
-                            text("INSERT INTO exchangerates (currency, from_date, to_date, average_value, min_value, max_value, ultimo_value) VALUES (:currency, :from_date, :to_date, :average_value, :min_value, :max_value, :ultimo_value)"),
-                            values,
-                        )
-                    result.append('OK ' + rate_to_get + '-' + from_date)
+                   res = client.insert_rows_json(table_id, values)
+                   result.append('OK ' + rate_to_get + '-' + from_date)
                 except Exception as e:
                     return 'Error: {}'.format(str(e))
-            else:
-                result.append('Err ' + rate_to_get + '-' + from_date)
+#            else:
+#                result.append('No data for ' + rate_to_get + '-' + from_date)
         else:
-            result.append('Err ' + rate_to_get + '-' + from_date)
+            result.append('No data for ' + rate_to_get + '-' + from_date)
         time.sleep(15)
-    #if len(result) > 0:
     return str(result)
-    #else:
-    #    return 'No data to get for any of our currencies'
